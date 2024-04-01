f551fe2ecaeae3492892ceba36304dd9dbbe007a
Arawn-Davies <19764552+Arawn-Davies@users.noreply.github.com>
2024-04-01 18:56:04 +0100

    Updated tests and system report

--- microCMDB.Test/CLITest.cs
+++ microCMDB.Test/CLITest.cs
@@ -7,8 +7,6 @@ namespace microCMDB.Test
     [TestClass]
     public class CLITest
     {
-
-
         [TestMethod]
         public void SoftwareStore()
         {
@@ -38,51 +36,94 @@ namespace microCMDB.Test
         {
             Db.CurrentDbContext = new Db();
             Host egHost = new Host { Name = "TestHost" };
-            Assert.IsTrue(Db.CurrentDbContext.Hosts.First().Name == "TestHost");
+            Assert.IsTrue(Db.CurrentDbContext.Hosts.First().Name == "TestHost", "Failed to store a Host");
         }
         [TestMethod]
         public void ServiceStore()
         {
             Db.CurrentDbContext = new Db();
             Service egService = new Service { Name = "TestService" };
-            Assert.IsTrue(Db.CurrentDbContext.Services.First().Name == "TestService");
+            Assert.IsTrue(Db.CurrentDbContext.Services.First().Name == "TestService", "Failed to store a Service");
         }
         [TestMethod]
         public void NetworkUserStore()
         {
             Db.CurrentDbContext = new Db();
             NetworkUser egNetworkUser = new NetworkUser { Name = "TestNetworkUser" };
-            Assert.IsTrue(Db.CurrentDbContext.NetworkUsers.First().Name == "TestNetworkUser");
+            Assert.IsTrue(Db.CurrentDbContext.NetworkUsers.First().Name == "TestNetworkUser", "Failed to store a Network User");
         }
 
         [TestMethod]
-        public void DeleteCITest()
+        public void DeleteTest()
         {
             Db.CurrentDbContext = new Db();
             Node egNode = new Node { Name = "TestNode", CPU_Arch = "x86_64", RAM = 32768, Description = "Put a more descriptive description here"};
-            Console.WriteLine("Example Node: " + egNode.ExportObject());
+            Console.WriteLine(egNode.ExportObject());
             CLI.Util.IO.DeleteEntity(egNode.DbTag);
             bool found = CLI.Util.Get.Find(egNode.DbTag);
             Assert.IsFalse(found, "Failed to delete Node entity from database");
         }
 
-        // Test the DbTag property for each entity type
         [TestMethod]
-        public void DbTagTest()
+        public void CITagTest()
+        {
+            Db.CurrentDbContext = new Db();
+            ConfigItem egConfigItem = new ConfigItem { Name = "TestConfigItem" };
+            Assert.IsTrue(egConfigItem.DbTag.StartsWith("CFG"));
+        }
+
+        [TestMethod]
+        public void NodeTagTest()
+        {
+            Db.CurrentDbContext = new Db();
+            Node egNode = new Node { Name = "TestNode" };
+            Assert.IsTrue(egNode.DbTag.StartsWith("NOD"));
+        }
+
+        [TestMethod]
+        public void HostTagTest()
+        {
+            Db.CurrentDbContext = new Db();
+            Host egHost = new Host { Name = "TestHost" };
+            Assert.IsTrue(egHost.DbTag.StartsWith("HST"));
+        }
+
+        [TestMethod]
+        public void ServiceTagTest()
+        {
+            Db.CurrentDbContext = new Db();
+            Service egService = new Service { Name = "TestService" };
+            Assert.IsTrue(egService.DbTag.StartsWith("SVC"));
+        }
+
+        [TestMethod]
+        public void SoftwareTagTest()
         {
             Db.CurrentDbContext = new Db();
             Software egSoftware = new Software { Name = "TestSoftware" };
             Assert.IsTrue(egSoftware.DbTag.StartsWith("STW"));
-            ConfigItem egConfigItem = new ConfigItem { Name = "TestConfigItem" };
-            Assert.IsTrue(egConfigItem.DbTag.StartsWith("CFG"));
-            Node egNode = new Node { Name = "TestNode" };
-            Assert.IsTrue(egNode.DbTag.StartsWith("NOD"));
-            Host egHost = new Host { Name = "TestHost" };
-            Assert.IsTrue(egHost.DbTag.StartsWith("HST"));
-            Service egService = new Service { Name = "TestService" };
-            Assert.IsTrue(egService.DbTag.StartsWith("SVC"));
+        }
+
+        [TestMethod]
+        public void NetworkUserTagTest()
+        {
+            Db.CurrentDbContext = new Db();
             NetworkUser egNetworkUser = new NetworkUser { Name = "TestNetworkUser" };
             Assert.IsTrue(egNetworkUser.DbTag.StartsWith("USR"));
+        }
+
+        
+
+        // Test the DbTag property for each entity type
+        [TestMethod]
+        public void DbTagTests()
+        {
+            CITagTest();
+            NodeTagTest();
+            HostTagTest();
+            ServiceTagTest();
+            SoftwareTagTest();
+            NetworkUserTagTest();
         }
     }
 }
